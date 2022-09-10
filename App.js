181eedbf7daf772b3582606c7e3c045fa168aade
David Barcenas <dabarcenas6921@gmail.com>
2022-09-10 16:29:51 -0400

    made explore page

--- App.js
+++ App.js
@@ -7,8 +7,8 @@ import {
   ProfileScreen,
   CameraScreen,
   RegisterScreen,
+  LoginScreen,
 } from "./screens";
-import { LoginScreen } from "./screens/LoginScreen/LoginScreen"
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { getApps, initializeApp } from "firebase/app";
@@ -44,7 +44,7 @@ function AppContainer() {
         <Tab.Screen name="Profile" component={ProfileScreen} />
         <Tab.Screen name="Camera" component={CameraScreen} />
         <Tab.Screen name="Register" component={RegisterScreen} />
-        <Tab.Screen name="Login" component={LoginScreen} />
+        {/* <Tab.Screen name="Login" component={LoginScreen} /> */}
       </Tab.Navigator>
     </NavigationContainer>
   );
