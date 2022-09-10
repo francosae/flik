a1aa2246d16012c3807b76370c8b4e37e5e91da3
Nana Kofi Okae <okaen@vcu.edu>
2022-09-10 10:43:27 -0400

    deleted og component folder with register/login and transfered files to screen folder

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NativeBaseProvider } from "native-base";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { ExploreScreen, FeedScreen, FriendScreen, ProfileScreen, CameraScreen} from "./screens";
+import { ExploreScreen, FeedScreen, FriendScreen, ProfileScreen, CameraScreen,RegisterScreen,LoginScreen} from "./screens";
 import { NavigationContainer } from "@react-navigation/native";
 import { FontAwesome } from '@expo/vector-icons';
 
@@ -28,17 +28,22 @@ export default function App() {
 const Tab = createBottomTabNavigator();
 
 function AppContainer(){
-  return(
+  return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Feed" screenOptions={{
-          headerShown: false
-        }}>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
         <Tab.Screen name="Feed" component={FeedScreen} />
         <Tab.Screen name="Explore" component={ExploreScreen} />
         <Tab.Screen name="Friends" component={FriendScreen} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
         <Tab.Screen name="Camera" component={CameraScreen} />
+        <Tab.Screen name="Register" component={RegisterScreen} />
+        <Tab.Screen name="Login" component={LoginScreen} />
       </Tab.Navigator>
     </NavigationContainer>
-  )
+  );
 }
