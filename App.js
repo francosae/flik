2ee390198aa2168d9d0a167d7d48e39f982853a0
Paul Franco <paulfranco.e@gmail.com>
2022-09-10 02:16:50 -0400

    Friends Page Work

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NativeBaseProvider } from "native-base";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ExploreScreen, FeedScreen, FriendScreen, ProfileScreen } from "./screens";
 import { NavigationContainer } from "@react-navigation/native";
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
   return (
@@ -17,8 +18,8 @@ function AppContainer(){
   return(
     <NavigationContainer>
       <Tab.Navigator initialRouteName="Feed" screenOptions={{
-    headerShown: false
-  }}>
+          headerShown: false
+        }}>
         <Tab.Screen name="Feed" component={FeedScreen} />
         <Tab.Screen name="Explore" component={ExploreScreen} />
         <Tab.Screen name="Friends" component={FriendScreen} />
--- screens/FriendScreen/FriendScreen.js
+++ screens/FriendScreen/FriendScreen.js
@@ -1,11 +1,60 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet  } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Heading } from 'native-base'
+import { Heading, HStack, Input, VStack } from 'native-base'
+
+const testData = [
+    {
+        name: 'Julia Sherman',
+        at: '@juliaroo',
+    },
+    {
+        name: 'Miss Lady',
+        at: '@miss',
+    },
+    {
+        name: 'Mark Keeper',
+        at: '@mark',
+    },
+    {
+        name: 'Person McPerson',
+        at: '@person',
+    }
+]
+
 export default function FriendScreen() {
   return (
     <SafeAreaView>
-      <Heading size="md">md</Heading>
+      <VStack space={4} alignItems="center">
+        <Text fontSize="xl">Your friends</Text>
+        <Input maxW="80%" size="md" placeholder="Search your friends" />
+      </VStack>
+
+      <VStack maxW="80%" alignItems="left">
+        <Text bold fontSize="md">MY FRIENDS</Text>
+      </VStack>
+
+      <VStack space={4} alignItems="center">
+        {testData.map((data, index) => {
+            return (
+                <HStack alignItems="center" key={index}>
+                <VStack space={1} alignItems="center">
+                    <Text fontSize='md'>{data.name}</Text>
+                    <Text fontSize='md'>{data.at}</Text>
+                </VStack>
+                <Image style={styles.profPic} source={{uri: 'https://placeimg.com/80/80/people'}} />
+                </HStack>
+            )
+        })}
+      </VStack  >
+
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+    profPic: {
+      width: 32,
+      height: 32,
+    },
+  });
