5ed4642cfb3c4103dc513be1f72d87cc399c264b
erico07181 <enriquearico@gmail.com>
2022-09-10 12:30:30 -0600

    reg work

--- screens/index.js
+++ screens/index.js
@@ -3,5 +3,13 @@ import FeedScreen from "./FeedScreen/FeedScreen";
 import FriendScreen from "./FriendScreen/FriendScreen";
 import ProfileScreen from "./ProfileScreen/ProfileScreen";
 import CameraScreen from "./CameraScreen/CameraScreen";
+import RegisterScreen from "./RegisterScreen/RegisterScreen";
 
-export {ExploreScreen, FeedScreen, FriendScreen, ProfileScreen, CameraScreen}
+export {
+  ExploreScreen,
+  FeedScreen,
+  FriendScreen,
+  ProfileScreen,
+  CameraScreen,
+  RegisterScreen,
+};
